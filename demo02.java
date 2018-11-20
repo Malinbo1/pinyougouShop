2c11fda0e8433746a2fa35a6c2f3c1244c6fde09
shen_glb <test_glb@shen.com>
2018-11-20 19:53:38 +0800

    实体类

---
+++ demo02.java
@@ -0,0 +1,11 @@
+public class demo2{
+   private int id;
+   private String name;
+   private boolean sex;
+   private Date birthday;
+
+   public demo2(){
+     System.out.println("这是无参构造");
+   }
+
+}
