66a55074be376564b9278aa3b4fe424eba38f685
shen_glb <test_glb@shen.com>
2018-11-20 20:25:54 +0800

    段誉上线了

--- demo01.java
+++ demo01.java
@@ -4,10 +4,19 @@ public class demo01{
      private int age;
      public static void main(String[]args){
         demo01 d=new demo01();
-	d.id="1";
+	d.id=1;
 	d.name="萧峰"
-	d.age="32";
+	d.age=32;
         System.out.println(d.id+" "+d.name+" "+d.age);
+	d.id=2;
+	d.name="段誉
+	
+
+	d.age=20;
+
+	System.out.println(d.id+" "+d.name+" "+d.age);
+
+
      }
 
 }
