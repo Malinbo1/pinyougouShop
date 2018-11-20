cbb180ac72d43462431cdebfd204ee8024a0dec9
GarlicSmalltwo <45063780+GarlicSmalltwo@users.noreply.github.com>
2018-11-20 20:20:05 +0800

    修改demo01
    为了测试push,又一次修改了demo01

--- demo01.java
+++ demo01.java
@@ -4,10 +4,13 @@ public class demo01{
      private int age;
      public static void main(String[]args){
         demo01 d=new demo01();
-	d.id="1";
-	d.name="萧峰"
-	d.age="32";
+	xxxxxxxxxxxxx
+	xxxxxxxxxxxxx
+	
+		
         System.out.println(d.id+" "+d.name+" "+d.age);
+	     
+	Recnetly,I like a TV named Tianlongbabu,there is a hero in Song，called Xiaofeng.I is spelled with him deeply.      
      }
 
 }
